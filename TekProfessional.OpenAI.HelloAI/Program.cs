358275e4df1ca0322a99dcbb9ba17b9c97aa5138
Daniel Crane <daniel.crane@live.com>
2023-03-25 14:44:07 -0700

    Add project files.

---
+++ TekProfessional.OpenAI.HelloAI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TekProfessional.OpenAI.HelloAI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

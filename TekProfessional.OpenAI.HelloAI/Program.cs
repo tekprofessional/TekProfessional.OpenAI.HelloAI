c788817aaa49cf4b365622e50e98b3f407ed22cd
Daniel Crane <daniel.crane@live.com>
2023-03-25 15:54:13 -0700

    Initialized sample app

--- TekProfessional.OpenAI.HelloAI/Program.cs
+++ TekProfessional.OpenAI.HelloAI/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace TekProfessional.OpenAI.HelloAI
+﻿using OpenAI.NET.Clients.OpenAIs;
+using OpenAI.NET.Models.Configurations;
+using OpenAI.NET.Models.Services.Foundations.Completions;
+using System.Text;
+
+namespace TekProfessional.OpenAI.HelloAI
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var openAiApiConfigurations = new ApiConfigurations
+            {
+                ApiKey = "",
+                OrganizationId = ""
+            };
+
+            var openAiClient = new OpenAIClient(openAiApiConfigurations);
+            var promptMessage = $"Human: I am TekProfessional, using the recently released Standard.AI.OpenAI nuget library to chat with you\r\n";
+            var inputCompletion = new Completion
+            {
+                Request = new CompletionRequest
+                {
+                    Prompt = new string[] { promptMessage },
+                    MaxTokens = 300,
+                    Model = "text-davinci-003"
+                }
+            };
+
+            Completion resultCompletion =
+                await openAiClient.Completions.PromptCompletionAsync(
+                    inputCompletion);
+
+            Console.WriteLine(promptMessage);
+            Array.ForEach(resultCompletion.Response.Choices, choice => Console.WriteLine(choice.Text));
         }
+
     }
 }
